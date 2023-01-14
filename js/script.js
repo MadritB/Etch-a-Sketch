b75266f2ec11cb6ac1063964ee6136240c9409a8
MadritB <t119004133@gmail.com>
2023-01-14 22:13:30 +0530

    Add function to leave color on grid element on hover
    When the mouse pointer hovers over the grid, it leaves a trail
    behind it. Also added the option to chose between black and
    random color.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,39 @@
 const gridContainer = document.querySelector(".grid.container");
 
+// Getting the side length of the container
+
 const containerStyle = getComputedStyle(gridContainer);
 const lengthString = containerStyle.height;
 const length = Number(lengthString.slice(0, -2));
 
+// Button to create grid
+
 const createButton = document.querySelector(".create.button");
 createButton.addEventListener("click", makeGrid);
+
+// Getting grid container color
+const containerColor = containerStyle.backgroundColor;
+
+// Changes color of grid element
+
+let random = true;
+
+const toggleColorButton = document.querySelector(".toggle.color");
+toggleColorButton.addEventListener("click",() => {random = !random} );
+
+function changeColor()
+{
+    if(!random)
+        this.style.backgroundColor = containerColor;
+    else
+    {
+        let r = (Math.floor(5000*Math.random()) % 255) + 1;
+        let g = (Math.floor(5000*Math.random()) % 255) + 1;
+        let b = (Math.floor(5000*Math.random()) % 255) + 1;
+
+        this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    }
+}
 
 // Creates a grid element and appends it to a container
 function makeGrid()
@@ -17,7 +45,7 @@ function makeGrid()
     }
 
     // Making new children
-    let num = 10;
+    let num = 20;
     side = ((length)/num);
     let i = 0;
     for(i = 0; i < num*num; i++)
@@ -27,6 +55,9 @@ function makeGrid()
         gridElement.classList.add("element");
         gridElement.style.height = `${side}px`;
         gridElement.style.width = `${side}px`;
+        gridElement.addEventListener("mouseover", changeColor);
         gridContainer.appendChild(gridElement);
     }
 }
+
+makeGrid();
